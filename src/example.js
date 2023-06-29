b0a6930d0862ff8f2a23b5145746275ea1cf2677
sumanthbondu <40199161+sumanthbondu@users.noreply.github.com>
2023-06-29 11:14:47 -0400

    Update example.js

--- src/example.js
+++ src/example.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/raja', (req, res) => {
-  res.send('Hello, This is Raja!')
+app.get('/praneeth', (req, res) => {
+  res.send('Hello, This is Praneeth!')
 })
 
 app.listen(port, () => {
